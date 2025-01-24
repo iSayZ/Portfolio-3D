35cb635d8c36c8fad9964b5ee4a2fd55c369e192
iSayZ <estrine.alexis@gmail.com>
2025-01-24 08:15:11 +0100

    fix: Hydratation error in Skills section fixed

--- src/components/sections/Skills/components.tsx
+++ src/components/sections/Skills/components.tsx
@@ -1,27 +1,40 @@
 "use client";
 
 import { useIsScreenLarge } from "@/hooks/useIsLargeScreen";
-import { SoftSkillsDesktop, HardSkillsDesktop } from "./components/SkillSlider";
-import { HardSkillsMobile, SoftSkillsMobile } from "./components/SkillsMobile";
+import dynamic from "next/dynamic";
+
+const HardSkillsDesktop = dynamic(
+  () => import("./components/SkillSlider").then((mod) => mod.HardSkillsDesktop),
+  { ssr: false },
+);
+const SoftSkillsDesktop = dynamic(
+  () => import("./components/SkillSlider").then((mod) => mod.SoftSkillsDesktop),
+  { ssr: false },
+);
+const HardSkillsMobile = dynamic(
+  () => import("./components/SkillsMobile").then((mod) => mod.HardSkillsMobile),
+  { ssr: false },
+);
+const SoftSkillsMobile = dynamic(
+  () => import("./components/SkillsMobile").then((mod) => mod.SoftSkillsMobile),
+  { ssr: false },
+);
 
 const Skills: React.FC = () => {
   const isLargeScreen = useIsScreenLarge(768);
 
   return (
-    <section
-      id="skills"
-      className={`w-full overflow-hidden ${isLargeScreen ? "space-y-6" : "space-y-24"}`}
-    >
+    <section id="skills" className="w-full overflow-hidden">
       {isLargeScreen ? (
-        <>
+        <div className="space-y-6">
           <SoftSkillsDesktop />
           <HardSkillsDesktop />
-        </>
+        </div>
       ) : (
-        <>
+        <div className="space-y-24">
           <HardSkillsMobile />
           <SoftSkillsMobile />
-        </>
+        </div>
       )}
     </section>
   );
