4f1d4174b9e9e0e21f8e24a88bc53a6ace1bca63
iSayZ <estrine.alexis@gmail.com>
2025-01-31 14:12:14 +0100

    feat: Jest implemented

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "jest";
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
+
+export default createJestConfig(config);
