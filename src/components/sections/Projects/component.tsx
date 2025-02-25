b51144832841f0e86fb6563b922d0ddcce420c30
iSayZ <estrine.alexis@gmail.com>
2025-02-25 12:14:18 +0100

    feat: Add projects under construction to the top of the project list

--- src/components/sections/Projects/component.tsx
+++ src/components/sections/Projects/component.tsx
@@ -3,14 +3,21 @@
 import { useIsScreenLarge } from "@/hooks/useIsLargeScreen";
 import { ProjectCard } from "./components/ProjectCard";
 import { projects } from "@/config/projects.config";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useProjectsStore } from "./store/useProjectStore";
 import FadeInOnScroll from "@/components/template/animations/FadeInOnScroll";
+import { Project } from "./types";
 
 export const Projects = () => {
   const { visibleProjects, setVisibleProjects } = useProjectsStore();
   const isLargeScreen = useIsScreenLarge(768);
+
+  const [sortedProjects, setSortedProjects] = useState<Project[]>(() => {
+    const projectsInConstruction = projects.filter(project => project.isInConstruction);
+    const finishedProjects = projects.filter(project => !project.isInConstruction);
+    return [...projectsInConstruction, ...finishedProjects];
+  });
 
   useEffect(() => {
     const minVisible = isLargeScreen ? 6 : 3;
@@ -30,7 +37,7 @@ export const Projects = () => {
           <h2 className="text-3xl font-bold text-center">Mes Projets</h2>
         </FadeInOnScroll>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 lg:gap-6">
-          {projects.slice(0, visibleProjects).map((project) => (
+          {sortedProjects.slice(0, visibleProjects).map((project) => (
             <FadeInOnScroll key={project.id} moveY={50}>
               <ProjectCard project={project} />
             </FadeInOnScroll>
@@ -38,7 +45,7 @@ export const Projects = () => {
         </div>
       </div>
       <div className="w-full flex justify-center gap-4">
-        {projects.length > visibleProjects && (
+        {sortedProjects.length > visibleProjects && (
           <FadeInOnScroll moveY={50}>
             <Button
               onClick={showMoreProjects}
